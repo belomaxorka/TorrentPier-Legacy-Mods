2199949a55b4349bbaac916a44c3c2df8aaea8ab
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-02-02 01:07:37 +0700

    Updated

--- [FULL] Парсер раздач by belomaxorka/(Скопировать содержимое в корень движка)/release.php
+++ [FULL] Парсер раздач by belomaxorka/(Скопировать содержимое в корень движка)/release.php
@@ -11,3 +11,7 @@ $user->session_start(array('req_login' => true));
 if (!IS_AM && !$bb_cfg['parser_for_all']) {
 	bb_die($lang['NOT_AUTHORISED']);
 }
+
+// Подключаем вспомогательные файлы
+require_once(INC_DIR . '/parser/random_int.php'); // Polyfill для random_int()
+require_once(INC_DIR . '/parser/userAgent.php'); // Генератор User-agent
