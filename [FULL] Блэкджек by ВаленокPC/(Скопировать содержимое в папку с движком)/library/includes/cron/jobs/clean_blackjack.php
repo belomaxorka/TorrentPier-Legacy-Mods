744d0adbe9f8e5932d11d6d3704f5b917fe452e9
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-02-03 00:40:42 +0700

    Updated

--- [FULL] Блэкджек by ВаленокPC/(Скопировать содержимое в папку с движком)/library/includes/cron/jobs/clean_blackjack.php
+++ [FULL] Блэкджек by ВаленокPC/(Скопировать содержимое в папку с движком)/library/includes/cron/jobs/clean_blackjack.php
@@ -4,25 +4,23 @@ if (!defined('BB_ROOT')) die(basename(__FILE__));
 
 $sql = DB()->fetch_rowset("SELECT * FROM " . BB_BLACKJACK . " ORDER BY `bj_date` DESC");
 
-foreach($sql as $row) {
+foreach ($sql as $row) {
+	switch ($row['bj_plstat']) {
+		case 'playing':
+			if (!$row['bj_gamer'] && !$row['bj_winner'] && ($row['bj_date'] < (TIMENOW - 60 * 15))) {
+				DB()->query("DELETE FROM " . BB_BLACKJACK . " WHERE bj_id = " . $row['bj_id']);
+			}
+			break;
+		case 'waiting':
+			if ($row['bj_gamer'] && !$row['bj_winner'] && ($row['bj_date'] < (TIMENOW - 60 * 15))) {
 
-    switch ($row['bj_plstat']) {
-        case 'playing':
-            if (!$row['bj_gamer'] && !$row['bj_winner'] && ($row['bj_date'] < (TIMENOW - 60 * 15))) {
-                DB()->query("DELETE FROM " . BB_BLACKJACK . " WHERE bj_id = ". $row['bj_id']);
-            }
-            break;
-        case 'waiting':
-            if ($row['bj_gamer'] && !$row['bj_winner'] && ($row['bj_date'] < (TIMENOW - 60 * 15))) {
-
-                DB()->query("DELETE FROM " . BB_BLACKJACK . " WHERE bj_id = ". $row['bj_id']);
-            }
-            break;
-        case 'finished':
-            if ($row['bj_gamer'] && $row['bj_winner'] && ($row['bj_date'] < (TIMENOW - 172800 * 2))) {
-                DB()->query("DELETE FROM " . BB_BLACKJACK . " WHERE bj_id = ". $row['bj_id']);
-            }
-
-            break;
-    }
+				DB()->query("DELETE FROM " . BB_BLACKJACK . " WHERE bj_id = " . $row['bj_id']);
+			}
+			break;
+		case 'finished':
+			if ($row['bj_gamer'] && $row['bj_winner'] && ($row['bj_date'] < (TIMENOW - 172800 * 2))) {
+				DB()->query("DELETE FROM " . BB_BLACKJACK . " WHERE bj_id = " . $row['bj_id']);
+			}
+			break;
+	}
 }
