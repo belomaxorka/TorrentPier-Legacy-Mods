e4d01f1099c3216301e50b4e64b02d18454b5417
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-02-04 01:33:01 +0700

    Update bj.php

--- [FULL] Блэкджек by ВаленокPC/(Скопировать содержимое в папку с движком)/library/ajax/bj.php
+++ [FULL] Блэкджек by ВаленокPC/(Скопировать содержимое в папку с движком)/library/ajax/bj.php
@@ -13,12 +13,20 @@ if ($sql) {
 	foreach ($sql as $row) {
 		if ($row['bj_plstat'] == 'waiting') {
 			$self = ($row['bj_placeholder'] == $userdata['username'] || $row['bj_gamer'] ? "disabled" : "");
+
+			$gamer = '';
+			if (!empty($row['bj_gamer'])) {
+				$gamer = profile_url(get_userdata($row['bj_gamer'], true));
+			} else {
+				$gamer = '--';
+			}
+
 			$html_w .= "<tr><td class=\"row1\" width=\"15%\" align=\"center\">" . profile_url(get_userdata($row['bj_placeholder'], true)) . "</td>\n
 							<td class=\"row1\" width=\"20%\" align=\"center\">" . bb_date($row["bj_date"]) . "</td>\n
-							<td class=\"row1\" width=\"15%\" align=\"center\">" . !empty($row['bj_gamer']) ? profile_url(get_userdata($row['bj_gamer'], true)) : '--' . "</td>\n
+							<td class=\"row1\" width=\"15%\" align=\"center\">" . $gamer . "</td>\n
 							<td class=\"row1\" width=\"40%\" align=\"center\">Ставка:&nbsp;<input type=\"button\" style=\"cursor: pointer !important; width: 70px; height: 18px; background-color: #" . $bb_cfg['bj_colors'][$row['bj_bet']] . "; color: #FFFFFF; font-weight: normal; border: 1px solid white\" $self value='" . $row["bj_bet"] . "' onclick=\"window.open('blackjack.php?takegame=$row[bj_id]', '', 'height=280, width=620, toolbar=no, status=no, scrollbars=no, resize=no, menubar=no'); return false;\"></td>\n";
 			$html_w .= "</tr>\n";
-			unset($self);
+			unset($self, $gamer);
 		}
 		if ($row['bj_plstat'] == 'finished') {
 			if ($bb_cfg['max_finish_show'] && ($finish_count > $bb_cfg['max_finish_show'])) {
@@ -40,12 +48,19 @@ if ($sql) {
 				}
 			}
 
+			$gamer = '';
+			if (!empty($row['bj_gamer'])) {
+				$gamer = profile_url(get_userdata($row['bj_gamer'], true));
+			} else {
+				$gamer = '--';
+			}
+
 			$html_f .= "<tr><td class=\"row1 gen\" $bgcolor width=\"15%\" align=\"center\">" . profile_url(get_userdata($row['bj_placeholder'], true)) . "</td>\n
 							<td class=\"row1 gen\" $bgcolor width=\"20%\" align=\"center\">" . bb_date($row["bj_date"]) . "</td>\n
-							<td class=\"row1 gen\" $bgcolor width=\"15%\" align=\"center\">" . !empty($row['bj_gamer']) ? profile_url(get_userdata($row['bj_gamer'], true)) : '--' . "</td>\n
+							<td class=\"row1 gen\" $bgcolor width=\"15%\" align=\"center\">" . $gamer . "</td>\n
 							<td class=\"row1 gen\" $bgcolor width=\"40%\" align=\"center\">Ставка:&nbsp;<input type=\"button\" style=\"cursor: pointer !important; width: 70px; height: 18px; background-color: #" . $bb_cfg['bj_colors'][$row['bj_bet']] . "; color: #FFFFFF; font-weight: normal; border: 1px solid white\" $self value='$row[bj_bet]' onclick=\"window.open('blackjack.php?takegame=$row[bj_id]', '', 'height=280, width=620, toolbar=no, status=no, scrollbars=no, resize=no, menubar=no'); return false;\">" . sprintf($lang['BJ']['GAME_WIN'], $winner, $row['bj_points'], $row['bj_gamewithid']) . "</td>\n";
 			$html_f .= "</tr>\n";
-			unset($self, $bgcolor, $winner);
+			unset($self, $bgcolor, $winner, $gamer);
 			$finish_count++;
 		}
 	}
