71acbdac9a854f2986f7c868f9b6e6c4e6184f4c
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-04-06 12:06:18 +0700

    Updated

--- [FULL] Торрентбар by _Xz_/(содержимое скопировать в папку с движком)/torrentbar/index.php
+++ [FULL] Торрентбар by _Xz_/(содержимое скопировать в папку с движком)/torrentbar/index.php
@@ -1,14 +1,12 @@
 <?php
-
-/*
-	Torrenbar for TorrentPier
-    Author _Xz_
+/**
+ * Torrenbar for TorrentPier
+ * Author: _Xz_
  */
 
 define('BB_ROOT', '.././');
-require(BB_ROOT .'common.php');
-require(INC_DIR .'class.torrentbar.php');
+require(BB_ROOT . 'common.php');
+require(INC_DIR . 'class.torrentbar.php');
 
-$user_id = (int) request_var('u', '');
-
-torrentbar ($user_id);
+$user_id = (int)request_var('u', '');
+torrentbar($user_id);
