14a5815e775c54482dc75118ce627df01aad7515
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-11-08 19:42:55 +0700

    Update invite.php

--- [FULL] Инвайты by DarkMod/root/invite.php
+++ [FULL] Инвайты by DarkMod/root/invite.php
@@ -1,24 +1,34 @@
 <?php
 
-define('IN_PHPBB', true);
+define('IN_FORUM', true);
+define('BB_SCRIPT', 'invite');
 define('BB_ROOT', './');
 require(BB_ROOT . 'common.php');
 require(INC_DIR . 'functions_group.php');
 
 $user->session_start(array('req_login' => true));
+$template->assign_vars(array('PAGE_TITLE' => $lang['INVITES']));
 
-$page_title = $lang['INVITES'];
-$template->assign_vars(array('PAGE_TITLE' => $page_title));
-$profiledata = get_userdata($userdata['user_id']);
-$btu = get_bt_userdata($userdata['user_id']);
-$user_rating = (!empty($btu['u_down_total']) && $btu['u_down_total'] > MIN_DL_FOR_RATIO) ? round((($btu['u_up_total'] + $btu['u_up_release'] + $btu['u_up_bonus']) / $btu['u_down_total']), 0) : 0;
-$regdate = $profiledata['user_regdate'];
+if (!$btu = get_bt_userdata($userdata['user_id'])) {
+	require_once(INC_DIR . 'functions_torrent.php');
+	if (!generate_passkey($userdata['user_id'], true)) {
+		bb_die(sprintf($lang['PASSKEY_ERR_EMPTY'], PROFILE_URL . $userdata['user_id']));
+	}
+	$btu = get_bt_userdata($userdata['user_id']);
+}
+
+$user_rating = get_bt_ratio($btu);
+if ($user_rating == null) {
+	$user_rating = 0;
+}
+
+$regdate = $userdata['user_regdate'];
 $user_age = max(0, (date('Y') * 12) + date('n') - (date('Y', $regdate) * 12) - date('n', $regdate));
 $date_end = time();
 $date_start = $date_end - 604800;
 
 // User group
-$view_user_id = $profiledata['user_id'];
+$view_user_id = $userdata['user_id'];
 $groups = array();
 $user_group = array();
 $sql = '
@@ -37,8 +47,13 @@ $sql = '
 	ORDER BY
 		g.group_name,
 		g.group_id';
-if (!($result = DB()->sql_query($sql))) bb_die('Could not read groups' . __LINE__ . ',' . __FILE__ . $sql);
-while ($group = DB()->sql_fetchrow($result)) $groups[] = $group;
+if (!($result = DB()->sql_query($sql))) {
+	bb_die('Could not read groups' . __LINE__ . ',' . __FILE__ . $sql);
+}
+
+while ($group = DB()->sql_fetchrow($result)) {
+	$groups[] = $group;
+}
 
 $user_group[0] = '0';
 if (count($groups) > 0) {
@@ -77,11 +92,19 @@ else {
 
 if (isset($_GET['mode']) && $_GET['mode'] == 'getinvite') {
 	$sql = 'SELECT COUNT(`invite_id`) AS `invites_count_week` FROM ' . BB_INVITES . ' WHERE `user_id`=' . $userdata['user_id'] . ' AND `generation_date`>=' . $date_start . ' AND `generation_date`<=' . $date_end;
-	if (!($result = DB()->sql_query($sql))) bb_die('Could not get a list of invites' . __LINE__ . ',' . __FILE__ . $sql);
+	if (!($result = DB()->sql_query($sql))) {
+		bb_die('Could not get a list of invites' . __LINE__ . ',' . __FILE__ . $sql);
+	}
+
 	$row = DB()->sql_fetchrowset($result);
 	$num_row = DB()->num_rows($result);
 	DB()->sql_freeresult($result);
-	if ($num_row > 0) $invites_count_week = $row[0]['invites_count_week']; else $invites_count_week = 0;
+
+	if ($num_row > 0) {
+		$invites_count_week = $row[0]['invites_count_week'];
+	} else {
+		$invites_count_week = 0;
+	}
 
 	$sql = 'SELECT `invites_count` FROM ' . BB_INVITE_RULES . ' WHERE `user_rating`<=' . $user_rating . ' AND `user_age`<=' . $user_age . ' AND (';
 	for ($i = 0; $i < count($user_group); $i++) {
@@ -90,22 +113,27 @@ if (isset($_GET['mode']) && $_GET['mode'] == 'getinvite') {
 			$sql = $sql . ' OR ';
 		}
 	}
+
 	$sql = $sql . ') ORDER BY `invites_count` DESC LIMIT 1';
-	if (!($result = DB()->sql_query($sql))) bb_die('Could not get a list of rules for the invite' . __LINE__ . ',' . __FILE__ . $sql);
+	if (!($result = DB()->sql_query($sql))) {
+		bb_die('Could not get a list of rules for the invite' . __LINE__ . ',' . __FILE__ . $sql);
+	}
+
 	$row = DB()->sql_fetchrowset($result);
 	$num_row = DB()->num_rows($result);
 	DB()->sql_freeresult($result);
+
 	if ($num_row > 0) {
 		if ($invites_count_week < $row[0]['invites_count']) {
 			$invite_code = substr(md5(time()), rand(1, 14), 16);
 			$sql = "INSERT INTO " . BB_INVITES . " (`invite_id`,`user_id`,`new_user_id`,`invite_code`,`active`,`generation_date`,`activation_date`) VALUES(null," . (int)$userdata['user_id'] . ",0,'" . $invite_code . "','1'," . time() . ",0)";
+
 			if (!DB()->sql_query($sql)) {
 				$message = $lang['CAN_GET_INVITE'] . '' . sprintf($lang['GO_TO_INVITE_LIST'], '<a href="invite.php">', '</a>');
-				bb_die($message);
 			} else {
 				$message = $lang['INVITE_GET_SUCCESSFULLY'] . '' . sprintf($lang['GO_TO_INVITE_LIST'], '<a href="invite.php">', '</a>');
-				bb_die($message);
 			}
+			bb_die($message);
 		}
 	} else {
 		$message = $lang['CAN_GET_INVITE'] . '' . sprintf($lang['GO_TO_INVITE_LIST'], '<a href="invite.php">', '</a>');
@@ -114,56 +142,80 @@ if (isset($_GET['mode']) && $_GET['mode'] == 'getinvite') {
 }
 
 $referend_by_sql = 'SELECT * FROM ' . BB_INVITES . ' WHERE `new_user_id` =' . $userdata['user_id'];
-if (!($referend_by_result = DB()->sql_query($referend_by_sql))) bb_die('Could not get a data of invites' . __LINE__ . ',' . __FILE__ . $referend_by_sql);
+if (!($referend_by_result = DB()->sql_query($referend_by_sql))) {
+	bb_die('Could not get a data of invites' . __LINE__ . ',' . __FILE__ . $referend_by_sql);
+}
+
 $referend_by_row = DB()->sql_fetchrow($referend_by_result);
 $referend_by_user_data = get_userdata($referend_by_row['user_id']);
 $num_referend_by_row = DB()->num_rows($referend_by_result);
+
 if ($num_referend_by_row !== 0) {
-	$template->assign_vars(array(
-		'REFEREND' => true,));
+	$template->assign_vars(array('REFEREND' => true));
 	$template->assign_block_vars('referend_by', array(
-		'REF_GENERATION_DATE' => date('d.m.Y H:i', $referend_by_row['generation_date']),
+		'REF_GENERATION_DATE' => bb_date($referend_by_row['generation_date'], 'd-M-y H:i'),
 		'REF_INVITE_CODE' => $referend_by_row['invite_code'],
-		'REF_USER' => '<a href="profile.php?mode=viewprofile&u=' . $referend_by_row['user_id'] . '">' . $referend_by_user_data['username'] . '</a>',
-		'REF_ACTIVATION_DATE' => date('d.m.Y H:i', $referend_by_row['activation_date'])
+		'REF_USER' => profile_url($referend_by_user_data),
+		'REF_ACTIVATION_DATE' => bb_date($referend_by_row['activation_date'], 'd-M-y H:i'),
 	));
 } else {
-	$template->assign_vars(array(
-		'REFEREND' => false,));
+	$template->assign_vars(array('REFEREND' => false));
 }
 
 $sql = 'SELECT * FROM ' . BB_INVITES . ' WHERE `user_id`=' . $userdata['user_id'] . ' ORDER BY `generation_date` DESC';
-if (!($result = DB()->sql_query($sql))) bb_die('Could not get a list of invites' . __LINE__ . ',' . __FILE__ . $sql);
+if (!($result = DB()->sql_query($sql))) {
+	bb_die('Could not get a list of invites' . __LINE__ . ',' . __FILE__ . $sql);
+}
+
 $invite_row = DB()->sql_fetchrowset($result);
 $num_invite_row = DB()->num_rows($result);
 DB()->sql_freeresult($result);
+
 if ($num_invite_row > 0) {
 	$template->assign_vars(array('INVITES_PRESENT' => true));
 	for ($i = 0; $i < $num_invite_row; $i++) {
 		$new_user_data = get_userdata($invite_row[$i]['new_user_id']);
 		$template->assign_block_vars('invite_row', array(
-				'GENERATION_DATE' => date('d.m.Y H:i', $invite_row[$i]['generation_date']),
-				'INVITE_CODE' => $invite_row[$i]['invite_code'],
-				'ACTIVE' => ($invite_row[$i]['active'] == '1') ? $lang['INVITE_ACTIV_YES'] : $lang['INVITE_ACTIV_NO'],
-				'NEW_USER' => ($invite_row[$i]['active'] == '1') ? '-' : '<a href="profile.php?mode=viewprofile&u=' . $invite_row[$i]['new_user_id'] . '">' . $new_user_data['username'] . '</a>',
-				'ACTIVATION_DATE' => ($invite_row[$i]['active'] == '1') ? '-' : date('d.m.Y H:i', $invite_row[$i]['activation_date']))
-		);
+			'GENERATION_DATE' => bb_date($invite_row[$i]['generation_date'], 'd-M-y H:i'),
+			'INVITE_CODE' => $invite_row[$i]['invite_code'],
+			'ACTIVE' => ($invite_row[$i]['active'] == '1') ? $lang['INVITE_ACTIV_YES'] : $lang['INVITE_ACTIV_NO'],
+			'NEW_USER' => ($invite_row[$i]['active'] == '1') ? '-' : profile_url($new_user_data),
+			'ACTIVATION_DATE' => ($invite_row[$i]['active'] == '1') ? '-' : bb_date($invite_row[$i]['activation_date'], 'd-M-y H:i'),
+		));
 	}
-} else $template->assign_vars(array('INVITES_PRESENT' => false));
+} else {
+	$template->assign_vars(array('INVITES_PRESENT' => false));
+}
 
 $sql = 'SELECT COUNT(`invite_id`) AS `invites_count_all` FROM ' . BB_INVITES . ' WHERE `user_id`=' . $userdata['user_id'];
-if (!($result = DB()->sql_query($sql))) bb_die('Could not get a list of invites' . __LINE__ . ',' . __FILE__ . $sql);
+if (!($result = DB()->sql_query($sql))) {
+	bb_die('Could not get a list of invites' . __LINE__ . ',' . __FILE__ . $sql);
+}
+
 $row = DB()->sql_fetchrowset($result);
 $num_row = DB()->num_rows($result);
 DB()->sql_freeresult($result);
-if ($num_row > 0) $invites_count_all = $row[0]['invites_count_all']; else $invites_count_all = 0;
+
+if ($num_row > 0) {
+	$invites_count_all = $row[0]['invites_count_all'];
+} else {
+	$invites_count_all = 0;
+}
 
 $sql = 'SELECT COUNT(`invite_id`) AS `invites_count_week` FROM ' . BB_INVITES . ' WHERE `user_id`=' . $userdata['user_id'] . ' AND `generation_date`>=' . $date_start . ' AND `generation_date`<=' . $date_end;
-if (!($result = DB()->sql_query($sql))) bb_die('Could not get a list of invites' . __LINE__ . ',' . __FILE__ . $sql);
+if (!($result = DB()->sql_query($sql))) {
+	bb_die('Could not get a list of invites' . __LINE__ . ',' . __FILE__ . $sql);
+}
+
 $row = DB()->sql_fetchrowset($result);
 $num_row = DB()->num_rows($result);
 DB()->sql_freeresult($result);
-if ($num_row > 0) $invites_count_week = $row[0]['invites_count_week']; else $invites_count_week = 0;
+
+if ($num_row > 0) {
+	$invites_count_week = $row[0]['invites_count_week'];
+} else {
+	$invites_count_week = 0;
+}
 
 $sql = 'SELECT `invites_count` FROM ' . BB_INVITE_RULES . ' WHERE `user_rating`<=' . $user_rating . ' AND `user_age`<=' . $user_age . ' AND (';
 for ($i = 0; $i < count($user_group); $i++) {
@@ -173,42 +225,52 @@ for ($i = 0; $i < count($user_group); $i++) {
 	}
 }
 $sql = $sql . ') ORDER BY `invites_count` DESC';
+if (!($result = DB()->sql_query($sql))) {
+	bb_die('Could not get a list of rules for the invite' . __LINE__ . ',' . __FILE__ . $sql);
+}
 
-if (!($result = DB()->sql_query($sql))) bb_die('Could not get a list of rules for the invite' . __LINE__ . ',' . __FILE__ . $sql);
 $row = DB()->sql_fetchrowset($result);
 $num_row = DB()->num_rows($result);
 DB()->sql_freeresult($result);
+
 if ($num_row > 0) {
 	$invites_may_get = $row[0]['invites_count'] - $invites_count_week;
-	if ($invites_may_get > 0) $template->assign_vars(array('CAN_INVITE' => false)); else $template->assign_vars(array('CAN_INVITE' => true));
+	if ($invites_may_get > 0) {
+		$template->assign_vars(array('CAN_INVITE' => false));
+	} else {
+		$template->assign_vars(array('CAN_INVITE' => true));
+	}
 } else {
 	$invites_may_get = 0;
 	$template->assign_vars(array('CAN_INVITE' => true));
 }
 
 $template->assign_vars(array(
-		'USER_RATING' => $user_rating,
-		'USER_AGE' => $user_age,
-		'USER_GROUP' => $user_group[0],
-		'INVITES_GETTED_ALL' => $invites_count_all,
-		'INVITES_GETTED_WEEK' => $invites_count_week,
-		'INVITES_MAY_GET' => $invites_may_get
-	)
-);
+	'USER_RATING' => $user_rating,
+	'USER_AGE' => $user_age,
+	'USER_GROUP' => $user_group[0],
+	'INVITES_GETTED_ALL' => $invites_count_all,
+	'INVITES_GETTED_WEEK' => $invites_count_week,
+	'INVITES_MAY_GET' => $invites_may_get
+));
 
 $sql = 'SELECT * FROM ' . BB_INVITE_RULES . ' ORDER BY `invites_count`';
-if (!($result = DB()->sql_query($sql))) bb_die('Could not get a list of rules for the invite' . __LINE__ . ',' . __FILE__ . $sql);
+if (!($result = DB()->sql_query($sql))) {
+	bb_die('Could not get a list of rules for the invite' . __LINE__ . ',' . __FILE__ . $sql);
+}
+
 $rule_row = DB()->sql_fetchrowset($result);
 $num_rule_row = DB()->num_rows($result);
 DB()->sql_freeresult($result);
+
 if ($num_rule_row > 0) {
 	for ($i = 0; $i < $num_rule_row; $i++) {
 		$template->assign_block_vars('rule_row', array(
-				'USER_RATING' => $rule_row[$i]['user_rating'],
-				'USER_AGE' => $rule_row[$i]['user_age'],
-				'USER_GROUP' => (get_groupname($select_name = false, $select_ary = false, $rule_row[$i]['user_group'])) ? (get_groupname($select_name = false, $select_ary = false, $rule_row[$i]['user_group'])) : $lang['ENY_USER'],
-				'INVITES_COUNT' => $rule_row[$i]['invites_count'])
-		);
+			'USER_RATING' => $rule_row[$i]['user_rating'],
+			'USER_AGE' => $rule_row[$i]['user_age'],
+			'USER_GROUP' => get_groupname(false, false, $rule_row[$i]['user_group']) ?: $lang['ENY_USER'],
+			'INVITES_COUNT' => $rule_row[$i]['invites_count']
+		));
 	}
 }
 
