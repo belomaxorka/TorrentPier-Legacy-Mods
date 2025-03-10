9bc671d825fd6aee83366cb5465405ea3323db28
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-03-10 20:25:46 +0300

    Update portal.php

--- [FULL] Портал by Zenden/library/ajax/portal.php
+++ [FULL] Портал by Zenden/library/ajax/portal.php
@@ -20,5 +20,5 @@ $row['leechers'] = isset($row['seeders']) ? $row['seeders'] : 0;
 $row['speed_up'] = isset($row['speed_up']) ? $row['speed_up'] : 0;
 $row['speed_down'] = isset($row['speed_down']) ? $row['speed_down'] : 0;
 
-$this->response['html'] = '<span class="seedmed">&uArr;' . $row['seeders'] . ' (' . ($row['speed_up'] ? humn_size(($row['speed_up']), 0, 'KB') . '/s' : '0 KB/s') . ')</span><span class="leechmed">&dArr;' . $row['leechers'] . ' (' . ($row['speed_down'] ? humn_size(($row['speed_down']), 0, 'KB') . '/s' : '0 KB/s') . ')</span>';
+$this->response['html'] = '<span class="seedmed">&uArr;' . $row['seeders'] . ' (' . ($row['speed_up'] ? humn_size(($row['speed_up']), 0, 'KB') . '/s' : '0 KB/s') . ')</span>&nbsp;<span class="leechmed">&dArr;' . $row['leechers'] . ' (' . ($row['speed_down'] ? humn_size(($row['speed_down']), 0, 'KB') . '/s' : '0 KB/s') . ')</span>';
 $this->response['topic_id'] = $topic_id;
