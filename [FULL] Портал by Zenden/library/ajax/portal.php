63c66c75c5ac6d9fe242d9cb70b9df68af908129
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-03-10 18:26:23 +0300

    Update portal.php

--- [FULL] Портал by Zenden/library/ajax/portal.php
+++ [FULL] Портал by Zenden/library/ajax/portal.php
@@ -1,14 +1,19 @@
 <?php
 
-if (!defined('IN_AJAX')) die(basename(__FILE__));
+if (!defined('IN_AJAX')) {
+    die(basename(__FILE__));
+}
 
-$topic_id = (int) $this->request['topic_id'];
+if (!$topic_id = (int)$this->request['topic_id']) {
+    $this->ajax_die('invalid topic_id (empty)');
+}
 
-$row = DB()->fetch_row("SELECT seeders, leechers, speed_up, speed_down
-			FROM " . BB_BT_TRACKER_SNAP . "
-			WHERE topic_id = $topic_id
-			LIMIT 1"
-		);
+$row = DB()->fetch_row("
+    SELECT seeders, leechers, speed_up, speed_down
+		FROM " . BB_BT_TRACKER_SNAP . "
+		WHERE topic_id = $topic_id
+		LIMIT 1"
+);
 
-$this->response['html'] = '<span class="seedmed">&uArr;'.$row['seeders'].' ('.($row['speed_up'] ? humn_size(($row['speed_up']), 0, 'KB').'/s' : '0 KB/s').')</span><span class="leechmed">&dArr;'.$row['leechers'].' ('.($row['speed_down'] ? humn_size(($row['speed_down']), 0, 'KB').'/s' : '0 KB/s').')</span>';
-$this->response['topic_id']	= $topic_id;
+$this->response['html'] = '<span class="seedmed">&uArr;' . $row['seeders'] . ' (' . ($row['speed_up'] ? humn_size(($row['speed_up']), 0, 'KB') . '/s' : '0 KB/s') . ')</span><span class="leechmed">&dArr;' . $row['leechers'] . ' (' . ($row['speed_down'] ? humn_size(($row['speed_down']), 0, 'KB') . '/s' : '0 KB/s') . ')</span>';
+$this->response['topic_id'] = $topic_id;
