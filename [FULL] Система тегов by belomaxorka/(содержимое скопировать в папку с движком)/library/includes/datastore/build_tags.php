4c2dccc9f45fa37ccc6d7116dd6b3d9d647de9b1
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-03-14 18:13:44 +0300

    Create build_tags.php

---
+++ [FULL] Система тегов by belomaxorka/(содержимое скопировать в папку с движком)/library/includes/datastore/build_tags.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2025 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
+
+if (!defined('BB_ROOT')) {
+    die(basename(__FILE__));
+}
+
+$sql = "SELECT * FROM " . BB_TAGS;
+$tags = [];
+
+foreach (DB()->fetch_rowset($sql) as $row) {
+    $tags[$row['tag_id']] = $row;
+}
+
+$this->store('tags', $tags);
