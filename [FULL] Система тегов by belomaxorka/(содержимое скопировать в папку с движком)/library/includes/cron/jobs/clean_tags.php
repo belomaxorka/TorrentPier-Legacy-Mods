a396a6ec198a989e3e012caa686c157ea553cb76
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-03-16 13:03:06 +0300

    Update clean_tags.php

--- [FULL] Система тегов by belomaxorka/(содержимое скопировать в папку с движком)/library/includes/cron/jobs/clean_tags.php
+++ [FULL] Система тегов by belomaxorka/(содержимое скопировать в папку с движком)/library/includes/cron/jobs/clean_tags.php
@@ -1,24 +1,24 @@
 <?php
 
 if (!defined('BB_ROOT')) {
-    die(basename(__FILE__));
+	die(basename(__FILE__));
 }
 
 $tags = get_topic_tags()['all_tags']; // Все теги
 
 $need_update_datastore = false;
 foreach ($tags as $tag) {
-    $row = DB()->fetch_row("
+	$row = DB()->fetch_row("
         SELECT COUNT(*) AS total FROM " . BB_TOPIC_TAGS . " WHERE tag_id = {$tag['tag_id']}
     ");
 
-    if ($row['total'] == 0) {
-        // Если tag_id отсутствует в bb_topic_tags, удаляем запись из bb_tags
-        DB()->sql_query("DELETE FROM " . BB_TAGS . " WHERE tag_id = {$tag['tag_id']}");
-        $need_update_datastore = true;
-    }
+	if ($row['total'] == 0) {
+		// Если tag_id отсутствует в bb_topic_tags, удаляем запись из bb_tags
+		DB()->sql_query("DELETE FROM " . BB_TAGS . " WHERE tag_id = {$tag['tag_id']}");
+		$need_update_datastore = true;
+	}
 }
 
 if ($need_update_datastore) {
-    $datastore->update('tags');
+	$datastore->update('tags');
 }
