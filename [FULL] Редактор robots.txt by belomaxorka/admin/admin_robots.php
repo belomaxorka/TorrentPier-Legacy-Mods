a3546f280cf42a42bab0186d6af9b2061e9a13ad
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-05-31 10:54:17 +0300

    Update admin_robots.php

--- [FULL] Редактор robots.txt by belomaxorka/admin/admin_robots.php
+++ [FULL] Редактор robots.txt by belomaxorka/admin/admin_robots.php
@@ -8,8 +8,8 @@
  */
 
 if (!empty($setmodules)) {
-    $module['MODS']['ROBOTS_TXT_EDITOR_TITLE'] = basename(__FILE__);
-    return;
+	$module['MODS']['ROBOTS_TXT_EDITOR_TITLE'] = basename(__FILE__);
+	return;
 }
 
 require __DIR__ . '/pagestart.php';
@@ -18,28 +18,28 @@ $robots_file = BB_ROOT . 'robots.txt';
 
 // Обработка сохранения
 if (isset($_POST['save'])) {
-    $robots_txt = isset($_POST['robots_txt']) ? $_POST['robots_txt'] : '';
+	$robots_txt = isset($_POST['robots_txt']) ? $_POST['robots_txt'] : '';
 
-    if (!is_writable($robots_file) && is_file($robots_file)) {
-        bb_die('File robots.txt is not writable #1');
-    }
+	if (!is_writable($robots_file) && is_file($robots_file)) {
+		bb_die('File robots.txt is not writable #1');
+	}
 
-    $bytes = file_put_contents($robots_file, $robots_txt);
-    if ($bytes === false) {
-        bb_die('Could not write robots.txt #2');
-    }
+	$bytes = file_put_contents($robots_file, $robots_txt);
+	if ($bytes === false) {
+		bb_die('Could not write robots.txt #2');
+	}
 
-    bb_die($lang['ROBOTS_TXT_UPDATED_SUCCESSFULLY'] . '<br /><br />' . sprintf($lang['CLICK_RETURN_ROBOTS_TXT_CONFIG'], '<a href="admin_robots.php">', '</a>') . '<br /><br />' . sprintf($lang['CLICK_RETURN_ADMIN_INDEX'], '<a href="index.php?pane=right">', '</a>'));
+	bb_die($lang['ROBOTS_TXT_UPDATED_SUCCESSFULLY'] . '<br /><br />' . sprintf($lang['CLICK_RETURN_ROBOTS_TXT_CONFIG'], '<a href="admin_robots.php">', '</a>') . '<br /><br />' . sprintf($lang['CLICK_RETURN_ADMIN_INDEX'], '<a href="index.php?pane=right">', '</a>'));
 }
 
 $current_content = '';
 if (is_file($robots_file)) {
-    $current_content = file_get_contents($robots_file);
+	$current_content = file_get_contents($robots_file);
 }
 
 $template->assign_vars(array(
-    'S_ACTION' => 'admin_robots.php',
-    'ROBOTS_TXT' => htmlCHR($current_content),
+	'S_ACTION' => 'admin_robots.php',
+	'ROBOTS_TXT' => htmlCHR($current_content),
 ));
 
 print_page('admin_robots.tpl', 'admin');
