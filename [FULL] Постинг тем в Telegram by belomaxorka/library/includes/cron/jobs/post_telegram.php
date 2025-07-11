87f4cde48df7c2eda5cf9736c79aa51543757942
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-07-11 21:26:18 +0300

    Upload

--- [FULL] Постинг тем в Telegram by belomaxorka/library/includes/cron/jobs/post_telegram.php
+++ [FULL] Постинг тем в Telegram by belomaxorka/library/includes/cron/jobs/post_telegram.php
@@ -1,198 +1,149 @@
 <?php
 
 if (!defined('BB_ROOT')) {
-	die(basename(__FILE__));
+    die(basename(__FILE__));
 }
+
+global $cron_runtime_log;
 
 $chatId = $bb_cfg['tg_group'];
 $botToken = $bb_cfg['tg_botkey'];
 
 if (empty($botToken) || empty($chatId)) {
-	return;
-}
-
-/**
- * Конвертация даты в RU формат
- *
- * @param string $format
- * @param int|string $timestamp
- * @param bool $nominative_month
- * @return false|string
- */
-function rudate($format, $timestamp = 0, $nominative_month = false)
-{
-	$timestamp = (int)$timestamp;
-	if (!$timestamp) {
-		$timestamp = time();
-	} elseif (!preg_match("/^[0-9]+$/", $timestamp)) {
-		$timestamp = strtotime($timestamp);
-	}
-
-	$F = $nominative_month ? array(1 =>
-		'Январь', 'Февраль', 'Март',
-		'Апрель', 'Май', 'Июнь',
-		'Июль', 'Август', 'Сентябрь',
-		'Октябрь', 'Ноябрь', 'Декабрь'
-	) : array(1 =>
-		'Января', 'Февраля', 'Марта',
-		'Апреля', 'Мая', 'Июня',
-		'Июля', 'Августа', 'Сентября',
-		'Октября', 'Ноября', 'Декабря'
-	);
-	$M = array(1 =>
-		'Янв', 'Фев', 'Мар',
-		'Апр', 'Май', 'Июн',
-		'Июл', 'Авг', 'Сен',
-		'Окт', 'Ноя', 'Дек'
-	);
-	$l = array(
-		'Воскресенье', 'Понедельник', 'Вторник',
-		'Среда', 'Четверг', 'Пятница', 'Суббота'
-	);
-	$D = array('Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб');
-
-	$format = str_replace("F", $F[date("n", $timestamp)], $format);
-	$format = str_replace("M", $M[date("n", $timestamp)], $format);
-	$format = str_replace("l", $l[date("w", $timestamp)], $format);
-	$format = str_replace("D", $D[date("w", $timestamp)], $format);
-
-	return date($format, $timestamp);
+    $cron_runtime_log[] = 'Ошибка: Не указан токен бота или chat_id';
+    return;
 }
 
 $sql = "
     SELECT
-        ad.attach_id, ad.filetime,
-        t.topic_title, t.topic_id, t.topic_image,
+        a.attach_id,
+        t.topic_title, t.topic_id, t.topic_image, t.island_cloud_android_url, t.island_cloud_comics_url, t.topic_edit_reason,
         p.post_time,
         pt.post_text,
         u.username, u.user_id,
         f.forum_name, f.forum_tg_thread_id
     FROM bb_topics AS t
-    LEFT JOIN bb_posts AS p ON (p.topic_id = t.topic_id)
+    LEFT JOIN bb_posts AS p ON (p.post_id = t.topic_first_post_id)
     LEFT JOIN bb_posts_text AS pt ON (pt.post_id = p.post_id)
     LEFT JOIN bb_users AS u ON (u.user_id = p.poster_id)
     LEFT JOIN bb_forums AS f ON (f.forum_id = t.forum_id)
     LEFT JOIN bb_attachments AS a ON (a.post_id = p.post_id)
-    LEFT JOIN bb_attachments_desc AS ad ON (ad.attach_id = a.attach_id)
-    LEFT JOIN bb_bt_torrents AS tr ON (tr.post_id = p.post_id)
     WHERE t.posted_tg_status = 0
     LIMIT 10
 ";
 
 foreach (DB()->fetch_rowset($sql) as $row) {
-	$params = [
-		'chat_id' => $chatId,
-		'parse_mode' => 'HTML'
-	];
+    $params = [
+        'chat_id' => $chatId,
+        'parse_mode' => 'HTML'
+    ];
 
-	$forumThreadId = (!empty($row['forum_tg_thread_id']) && $row['forum_tg_thread_id'] > 0) ? $row['forum_tg_thread_id'] : false;
-	if (!$forumThreadId && $bb_cfg['tg_skip_forums_without_thread_id']) {
-		continue;
-	}
-	$isRelease = !empty($row['attach_id']);
-	$hasTopicImage = !empty($row['topic_image']) ? $row['topic_image'] : false;
+    $forumThreadId = (!empty($row['forum_tg_thread_id']) && $row['forum_tg_thread_id'] > 0) ? $row['forum_tg_thread_id'] : false;
+    if ($bb_cfg['tg_skip_forums_without_thread_id'] && !$forumThreadId) {
+        continue; // Пропускаем темы без forum_tg_thread_id
+    }
+    $isRelease = !empty($row['attach_id']) || !empty($row['island_cloud_android_url']) || !empty($row['island_cloud_comics_url']);
+    $hasTopicImage = !empty($row['topic_image']) ? $row['topic_image'] : false;
 
-	// Префиксы
-	$prefixes = '';
-	if (defined('BB_PREFIXES')) {
-		$topic_prefixes = get_prefixes_list($row['topic_id'])['topic_prefixes'];
-		$topic_prefixes = array_map(function ($prefix) {
-			return $prefix['prefix_name'];
-		}, $topic_prefixes);
-		$prefixes = ' ' . implode(' ', $topic_prefixes);
-	}
+    // Префиксы
+    $prefixes = '';
+    if (defined('BB_PREFIXES')) {
+        $topic_prefixes = get_prefixes_list($row['topic_id'])['topic_prefixes'];
+        $topic_prefixes = array_map(fn($prefix) => $prefix['prefix_name'], $topic_prefixes);
+        $prefixes = ' ' . implode(' ', $topic_prefixes);
+    }
 
-	// Теги
-	$tags = '';
-	if (defined('BB_TAGS')) {
-		$topic_tags = get_tags_list($row['topic_id'])['topic_tags'];
-		$topic_tags = array_map(function ($tag) {
-			return '#' . str_replace([' ', '-', ',', '.', "'"], '_', $tag['tag_name']);
-		}, $topic_tags);
-		$tags = implode(' ', $topic_tags);
-	}
+    // Теги
+    $tags = '';
+    if (defined('BB_TAGS')) {
+        $topic_tags = get_tags_list($row['topic_id'])['topic_tags'];
+        $topic_tags = array_map(fn($tag) => '#' . str_replace([' ', '-', ',', '.', "'"], '_', $tag['tag_name']), $topic_tags);
+        $tags = implode(' ', $topic_tags);
+    }
 
-	// Название темы
-	if ($forumThreadId) {
-		$message = "<a href=\"" . FULL_URL . TOPIC_URL . $row['topic_id'] . "\">" . htmlCHR($row['topic_title']) . "</a>" . $prefixes . "\n\n";
-	} else {
-		$message = "<a href=\"" . FULL_URL . TOPIC_URL . $row['topic_id'] . "\">" . htmlCHR($row['forum_name']) . " :: " . htmlCHR($row['topic_title']) . "</a>" . $prefixes . "\n\n";
-	}
+    // Название темы
+    if ($forumThreadId) {
+        $message = "<a href=\"" . FULL_URL . TOPIC_URL . $row['topic_id'] . "\">" . htmlCHR($row['topic_title']) . "</a>" . $prefixes;
+    } else {
+        $message = "<a href=\"" . FULL_URL . TOPIC_URL . $row['topic_id'] . "\">" . htmlCHR($row['forum_name']) . " :: " . htmlCHR($row['topic_title']) . "</a>" . $prefixes;
+    }
 
-	// Текст сообщения
-	if ($isRelease) {
-		//
-		// Раздача
-		//
+    // Текст сообщения
+    if ($isRelease) {
+        //
+        // Раздача
+        //
 
-		$message .= "<b>Автор темы:</b> <a href=\"" . FULL_URL . PROFILE_URL . $row['user_id'] . "\">" . htmlCHR($row['username']) . "</a>";
-		if ($row['post_time'] == $row['filetime']) {
-			$message .= "\n<b>Релиз добавлен:</b> " . rudate("j M Y G:i:s", $row['post_time']) . " (GMT +3)";
-		} elseif ($row['post_time'] < $row['filetime']) {
-			$message .= "\n<b>Релиз обновлен:</b> " . rudate("j M Y G:i:s", $row['filetime']) . " (GMT +3)";
-		}
-		$message .= "\nРазмер: " . humn_size($row['size']);
+        // Автор темы
+        // $message .= "\n\n<b>Автор темы:</b> <a href=\"" . FULL_URL . PROFILE_URL . $row['user_id'] . "\">" . htmlCHR($row['username']) . "</a>";
 
-		// Комментарий
-		if (!empty($row['comment'])) {
-			$message .= "\n\n<b>Комментарий:</b> " . htmlCHR($row['comment']);
-		}
-	} else {
-		//
-		// Обычное сообщение
-		//
+        // Ссылка на Android-приложение
+        $islandAndroid = !empty($row['island_cloud_android_url']) ? $row['island_cloud_android_url'] : false;
+        if ($islandAndroid) {
+            $message .= "\n\n<b>Андроид порт:</b> <a href=\"" . $islandAndroid . "\">" . $islandAndroid . "</a>";
+        }
 
-		if (!function_exists('strip_bbcode')) {
-			require INC_DIR . '/bbcode.php';
-		}
+        // Причина редактирования темы
+        if (!empty($row['topic_edit_reason'])) {
+            $message .= "\n\n<b>Причина редактирования:</b> " . htmlCHR($row['topic_edit_reason']);
+        }
+    } else {
+        //
+        // Обычное сообщение
+        //
 
-		$message .= strip_bbcode($row['post_text']);
-	}
+        if (!function_exists('strip_bbcode')) {
+            require INC_DIR . '/bbcode.php';
+        }
 
-	// Вывод списка тегов
-	if ($tags) {
-		$message .= "\n\n$tags";
-	}
+        $message .= "\n\n" . strip_bbcode($row['post_text']);
+        $message = preg_replace('/https?:\/\/[^\s]+\/' . preg_quote(hide_bb_path($bb_cfg['ajax_upload_posting_images_path']), '/') . '[^\s]+/', '', $message);
+    }
 
-	// Выбираем тип сообщения
-	if ($hasTopicImage) {
-		$imageLink = $hasTopicImage;
-		$params['photo'] = $imageLink;
-		$params['caption'] = $message;
-		$apiUrl = "https://api.telegram.org/bot$botToken/sendPhoto";
-	} else {
-		$params['text'] = $message;
-		$apiUrl = "https://api.telegram.org/bot$botToken/sendMessage";
-	}
+    // Вывод списка тегов
+    if ($tags) {
+        $message .= "\n\n$tags";
+    }
 
-	// Убедимся, что сообщение в правильной кодировке
-	$message = mb_convert_encoding($message, 'UTF-8', 'auto');
+    // Убедимся, что сообщение в правильной кодировке
+    $message = mb_convert_encoding($message, 'UTF-8', 'auto');
 
-	// Определяем message_thread_id для конкретного форума
-	if ($forumThreadId) {
-		$params['message_thread_id'] = $forumThreadId;
-	}
+    // Выбираем тип сообщения
+    if ($hasTopicImage) {
+        $imageLink = $hasTopicImage;
+        $params['photo'] = $imageLink;
+        $params['caption'] = str_short($message, 1024);
+        $apiUrl = "https://api.telegram.org/bot$botToken/sendPhoto";
+    } else {
+        $params['text'] = str_short($message, 4096);
+        $apiUrl = "https://api.telegram.org/bot$botToken/sendMessage";
+    }
 
-	// Инициализация cURL
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $apiUrl);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+    // Определяем message_thread_id для конкретного форума
+    if ($forumThreadId) {
+        $params['message_thread_id'] = $forumThreadId;
+    }
 
-	// Отправка запроса
-	$response = curl_exec($ch);
-	if (curl_errno($ch)) {
-		$cron_runtime_log[] = 'Ошибка при отправке запроса: ' . curl_error($ch);
-	} else {
-		$decodedResponse = json_decode($response, true);
-		if ($decodedResponse && $decodedResponse['ok']) {
-			DB()->sql_query("UPDATE `bb_topics` SET `posted_tg_status` = 1 WHERE `topic_id` = " . (int)$row['topic_id'] . " LIMIT 1");
-		} else {
-			$cron_runtime_log[] = 'Ошибка при публикации поста: ' . (isset($decodedResponse['description']) ? $decodedResponse['description'] : 'Неизвестная ошибка');
-		}
-	}
+    // Инициализация cURL
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
 
-	// Закрываем соединение
-	curl_close($ch);
+    // Отправка запроса
+    $response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        $cron_runtime_log[] = 'Ошибка при отправке запроса: ' . curl_error($ch);
+    } else {
+        $decodedResponse = json_decode($response, true);
+        if ($decodedResponse && $decodedResponse['ok']) {
+            DB()->sql_query("UPDATE `bb_topics` SET `posted_tg_status` = 1 WHERE `topic_id` = " . (int)$row['topic_id'] . " LIMIT 1");
+        } else {
+            $cron_runtime_log[] = 'Ошибка при публикации поста: ' . $decodedResponse['description'] ?? 'Неизвестная ошибка';
+        }
+    }
+
+    // Закрываем соединение
+    curl_close($ch);
 }
