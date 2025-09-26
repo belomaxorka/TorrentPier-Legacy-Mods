3b0fad2ae28c36907a40ac753ab1be896c3dd0b6
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-09-26 11:02:52 +0300

    Update functions_thanks.php

--- [FULL] Спасибо за раздачу и оценка релиза by RoadTrain/root/library/includes/functions_thanks.php
+++ [FULL] Спасибо за раздачу и оценка релиза by RoadTrain/root/library/includes/functions_thanks.php
@@ -2,20 +2,22 @@
 function get_user_thanks($uid)
 {
 	$uid = (int)$uid;
-	$query = "SELECT COUNT(user_id) AS thanks FROM " . BB_ATTACHMENTS_RATING . " WHERE user_id = $uid";
-	$result = DB()->query($query);
+	$row = DB()->fetch_row("
+			SELECT COUNT(user_id) AS thanks
+				FROM " . BB_ATTACHMENTS_RATING . "
+			WHERE user_id = $uid");
 
-	return mysqli_fetch_assoc($result)['thanks'] ?? 0;
+	return $row['thanks'] ?? 0;
 }
 
 function get_user_thanked($uid)
 {
 	$uid = (int)$uid;
-	$query = "SELECT COUNT(r.user_id) AS thanked
+	$row = DB()->fetch_row("
+			SELECT COUNT(r.user_id) AS thanked
 				FROM " . BB_ATTACHMENTS_RATING . " r
-    			LEFT JOIN bb_attachments a ON (a.attach_id = r.attach_id)
-    			WHERE a.user_id_1 = $uid AND r.thanked = 1";
-	$result = DB()->query($query);
+				LEFT JOIN bb_attachments a ON (a.attach_id = r.attach_id)
+			WHERE a.user_id_1 = $uid AND r.thanked = 1");
 
-	return mysqli_fetch_assoc($result)['thanked'] ?? 0;
+	return $row['thanked'] ?? 0;
 }
