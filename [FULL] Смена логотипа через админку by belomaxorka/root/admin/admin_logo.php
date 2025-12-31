1c77c0cf2c5243af6f7fc4dbce8b0ec8e2e2da99
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-12-31 16:51:18 +0300

    Updated

--- [FULL] Смена логотипа через админку by belomaxorka/root/admin/admin_logo.php
+++ [FULL] Смена логотипа через админку by belomaxorka/root/admin/admin_logo.php
@@ -17,8 +17,39 @@ require __DIR__ . '/pagestart.php';
 $logo_dir = BB_PATH . '/styles/images/logo/';
 $logo_url = 'styles/images/logo/';
 
+// Delete logo
+if (isset($_POST['delete_logo'], $_POST['confirm_delete'])) {
+	$logo_to_delete = isset($_POST['logo_to_delete']) ? $_POST['logo_to_delete'] : '';
+	$default_logo = 'styles/images/logo/logo.png';
+
+	if ($logo_to_delete && $logo_to_delete !== $default_logo) {
+		$delete_path = BB_PATH . '/' . $logo_to_delete;
+
+		if (file_exists($delete_path) && is_file($delete_path)) {
+			// Check if file is in logo directory
+			$real_delete_path = realpath($delete_path);
+			$real_logo_dir = realpath($logo_dir);
+
+			// Replace str_starts_with() with substr() or strpos()
+			if ($real_delete_path && $real_logo_dir && strpos($real_delete_path, $real_logo_dir) === 0) {
+				if (unlink($delete_path)) {
+					// If deleted logo was current, reset to default
+					$current = isset($bb_cfg['site_logo']) ? $bb_cfg['site_logo'] : '';
+					if ($current === $logo_to_delete) {
+						bb_update_config(array('site_logo' => $default_logo));
+					}
+					bb_die($lang['LOGO_DELETED'] . '<br /><br />' . sprintf($lang['CLICK_RETURN_ADMIN_INDEX'], '<a href="admin_logo.php">', '</a>'));
+				}
+			}
+		}
+	}
+
+	bb_die($lang['LOGO_DELETE_ERROR'] . '<br /><br />' . sprintf($lang['CLICK_RETURN_ADMIN_INDEX'], '<a href="admin_logo.php">', '</a>'));
+}
+
+// Upload new logo
 if (isset($_FILES['logo_upload']) && $_FILES['logo_upload']['error'] === UPLOAD_ERR_OK) {
-	$allowed_ext = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'webp'];
+	$allowed_ext = array('png', 'jpg', 'jpeg', 'gif', 'svg', 'webp');
 	$file_ext = strtolower(pathinfo($_FILES['logo_upload']['name'], PATHINFO_EXTENSION));
 
 	if (in_array($file_ext, $allowed_ext)) {
@@ -26,7 +57,7 @@ if (isset($_FILES['logo_upload']) && $_FILES['logo_upload']['error'] === UPLOAD_
 		$upload_path = $logo_dir . $filename;
 
 		if (move_uploaded_file($_FILES['logo_upload']['tmp_name'], $upload_path)) {
-			bb_update_config(['site_logo' => $logo_url . $filename]);
+			bb_update_config(array('site_logo' => $logo_url . $filename));
 			bb_die($lang['LOGO_UPLOADED'] . '<br /><br />' . sprintf($lang['CLICK_RETURN_ADMIN_INDEX'], '<a href="admin_logo.php">', '</a>'));
 		}
 	}
@@ -34,14 +65,14 @@ if (isset($_FILES['logo_upload']) && $_FILES['logo_upload']['error'] === UPLOAD_
 	$logo_path = (isset($_POST['logo_select'])) ? trim($_POST['logo_select']) : '';
 
 	if ($logo_path) {
-		bb_update_config(['site_logo' => $logo_path]);
+		bb_update_config(array('site_logo' => $logo_path));
 		bb_die($lang['CONFIG_UPDATED'] . '<br /><br />' . sprintf($lang['CLICK_RETURN_ADMIN_INDEX'], '<a href="index.php?pane=right">', '</a>'));
 	}
 }
 
 $current_logo = (isset($bb_cfg['site_logo'])) ? trim($bb_cfg['site_logo']) : 'styles/images/logo/logo.png';
 
-$logos = [];
+$logos = array();
 if (is_dir($logo_dir)) {
 	$files = scandir($logo_dir);
 	foreach ($files as $file) {
@@ -51,19 +82,23 @@ if (is_dir($logo_dir)) {
 	}
 }
 
-$template->assign_vars([
+$default_logo = 'styles/images/logo/logo.png';
+
+$template->assign_vars(array(
 	'S_LOGO_ACTION' => 'admin_logo.php',
 	'CURRENT_LOGO' => $current_logo,
 	'CURRENT_LOGO_PREVIEW' => '../' . $current_logo,
-]);
+	'DEFAULT_LOGO' => $default_logo,
+));
 
 foreach ($logos as $logo) {
-	$template->assign_block_vars('logos', [
+	$template->assign_block_vars('logos', array(
 		'LOGO_PATH' => $logo,
 		'LOGO_PATH_PREVIEW' => '../' . $logo,
 		'LOGO_NAME' => basename($logo),
 		'SELECTED' => ($logo === $current_logo) ? 'checked' : '',
-	]);
+		'IS_DEFAULT' => ($logo === $default_logo),
+	));
 }
 
 print_page('admin_logo.tpl', 'admin');
--- [FULL] Смена логотипа через админку by belomaxorka/root/styles/templates/admin/admin_logo.tpl
+++ [FULL] Смена логотипа через админку by belomaxorka/root/styles/templates/admin/admin_logo.tpl
@@ -9,23 +9,24 @@
 <table class="forumline">
 <col class="row1">
 <col class="row2">
+<col class="row1">
 <tr>
-	<th colspan="2">{L_CURRENT_LOGO}</th>
+	<th colspan="3">{L_CURRENT_LOGO}</th>
 </tr>
 <tr>
-	<td colspan="2" class="tCenter">
+	<td colspan="3" class="tCenter">
 		<img src="{CURRENT_LOGO_PREVIEW}" alt="Current Logo" style="max-width: 300px; max-height: 100px;">
 	</td>
 </tr>
 <tr>
-	<th colspan="2">{L_UPLOAD_NEW_LOGO}</th>
+	<th colspan="3">{L_UPLOAD_NEW_LOGO}</th>
 </tr>
 <tr>
 	<td><h4>{L_SELECT_FILE}</h4><h6>{L_ALLOWED_FORMATS}</h6></td>
-	<td><input type="file" name="logo_upload" accept="image/*" /></td>
+	<td colspan="2"><input type="file" name="logo_upload" accept="image/*" /></td>
 </tr>
 <tr>
-	<th colspan="2">{L_SELECT_EXISTING_LOGO}</th>
+	<th colspan="3">{L_SELECT_EXISTING_LOGO}</th>
 </tr>
 <!-- BEGIN logos -->
 <tr>
@@ -38,10 +39,15 @@
 	<td class="tCenter">
 		<img src="{logos.LOGO_PATH_PREVIEW}" alt="{logos.LOGO_NAME}" style="max-width: 200px; max-height: 80px;">
 	</td>
+	<td class="tCenter">
+		<!-- IF not logos.IS_DEFAULT -->
+		<button type="button" class="liteoption" onclick="deleteLogo('{logos.LOGO_PATH}', '{logos.LOGO_NAME}')">{L_DELETE}</button>
+		<!-- ENDIF -->
+	</td>
 </tr>
 <!-- END logos -->
 <tr>
-	<td class="catBottom" colspan="2">
+	<td class="catBottom" colspan="3">
 		<input type="submit" name="submit" value="{L_SUBMIT}" class="mainoption" />&nbsp;&nbsp;
 		<input type="reset" value="{L_RESET}" class="liteoption" />
 	</td>
@@ -49,5 +55,21 @@
 </table>
 
 </form>
+
+<!-- Delete form -->
+<form id="deleteLogoForm" action="{S_LOGO_ACTION}" method="post" style="display: none;">
+	<input type="hidden" name="logo_to_delete" id="logo_to_delete" value="" />
+	<input type="hidden" name="delete_logo" value="1" />
+	<input type="hidden" name="confirm_delete" value="1" />
+</form>
+
+<script>
+function deleteLogo(logoPath, logoName) {
+	if (confirm('{L_LOGO_DELETE_CONFIRM}'.replace('%s', logoName))) {
+		document.getElementById('logo_to_delete').value = logoPath;
+		document.getElementById('deleteLogoForm').submit();
+	}
+}
+</script>
 
 <br clear="all" />
