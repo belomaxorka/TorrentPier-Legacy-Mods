ab9a0e2cc85cbbad36a18e488aa964c4e16100fd
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2026-02-28 22:28:45 +0700

    Update admin_ls.php

--- [FULL] Чтение личных сообщений пользователей by Exile/Для LTS и v2.1.5/admin_ls.php
+++ [FULL] Чтение личных сообщений пользователей by Exile/Для LTS и v2.1.5/admin_ls.php
@@ -28,9 +28,9 @@ if ($pm_count['total'] > 50) {
     $pagination = generate_pagination('admin_ls.php?', $pm_count['total'], $bb_cfg['posts_per_page'], $start);
 }
 
-$template->assign_vars([
+$template->assign_vars(array(
     'PAGINATION' => $pagination,
-]);
+));
 
 $sql = 'SELECT u.username AS username_1, u.user_id AS user_id_1, u.user_rank AS user_rank_1, u2.username AS username_2, u2.user_id AS user_id_2, u2.user_rank AS user_rank_2, pm.privmsgs_date, pm.privmsgs_ip, pm.privmsgs_type, pmt.privmsgs_text
 	FROM ' . BB_PRIVMSGS . ' pm, ' . BB_PRIVMSGS_TEXT . ' pmt, ' . BB_USERS . ' u, ' . BB_USERS . ' u2
@@ -46,27 +46,26 @@ $result = DB()->sql_query($sql);
 $row_counter = 0;
 while ($pm_text = DB()->sql_fetchrow($result)) {
     $row_class = !($row_counter % 2) ? 'row1' : 'row2';
-    
-    // Определяем статус сообщения
-    $status_map = [
-        PRIVMSGS_READ_MAIL => 'Прочитано',
-        PRIVMSGS_NEW_MAIL => 'Новое',
-        PRIVMSGS_UNREAD_MAIL => 'Непрочитано',
-        PRIVMSGS_SAVED_IN_MAIL => 'Сохранено (вх.)',
+
+    $status_map = array(
+        PRIVMSGS_READ_MAIL      => 'Прочитано',
+        PRIVMSGS_NEW_MAIL       => 'Новое',
+        PRIVMSGS_UNREAD_MAIL    => 'Непрочитано',
+        PRIVMSGS_SAVED_IN_MAIL  => 'Сохранено (вх.)',
         PRIVMSGS_SAVED_OUT_MAIL => 'Сохранено (исх.)',
-    ];
-    $status = $status_map[$pm_text['privmsgs_type']] ?? 'Неизвестно';
+    );
+    $status = isset($status_map[$pm_text['privmsgs_type']]) ? $status_map[$pm_text['privmsgs_type']] : 'Неизвестно';
     $status_class = ($pm_text['privmsgs_type'] == PRIVMSGS_NEW_MAIL) ? 'colorRed' : 'colorGray';
-    
-    $template->assign_block_vars('pmrow', [
+
+    $template->assign_block_vars('pmrow', array(
         'ROW_CLASS' => $row_class,
-        'FROM' => profile_url(['username' => $pm_text['username_1'], 'user_id' => $pm_text['user_id_1'], 'user_rank' => $pm_text['user_rank_1']]),
-        'TO' => profile_url(['username' => $pm_text['username_2'], 'user_id' => $pm_text['user_id_2'], 'user_rank' => $pm_text['user_rank_2']]),
-        'DATE' => bb_date($pm_text['privmsgs_date']),
-        'STATUS' => '<span class="' . $status_class . '">' . $status . '</span>',
-        'IP' => ($pm_text['privmsgs_ip'] != '7f000001') ? TorrentPier\Helpers\IPHelper::decode($pm_text['privmsgs_ip']) : '0.0.0.0',
-        'MESSAGE' => '<div class="post_wrap"><div class="post_body">' . bbcode2html($pm_text['privmsgs_text']) . '</div></div>',
-    ]);
+        'FROM'      => profile_url(array('username' => $pm_text['username_1'], 'user_id' => $pm_text['user_id_1'], 'user_rank' => $pm_text['user_rank_1'])),
+        'TO'        => profile_url(array('username' => $pm_text['username_2'], 'user_id' => $pm_text['user_id_2'], 'user_rank' => $pm_text['user_rank_2'])),
+        'DATE'      => bb_date($pm_text['privmsgs_date']),
+        'STATUS'    => '<span class="' . $status_class . '">' . $status . '</span>',
+        'IP'        => ($pm_text['privmsgs_ip'] != '7f000001') ? TorrentPier\Helpers\IPHelper::decode($pm_text['privmsgs_ip']) : '0.0.0.0',
+        'MESSAGE'   => '<div class="post_wrap"><div class="post_body">' . bbcode2html($pm_text['privmsgs_text']) . '</div></div>',
+    ));
 
     $row_counter++;
 }
